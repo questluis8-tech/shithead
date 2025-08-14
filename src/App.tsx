298ef7a26f84d80db572c835a4155294567d8b7d
questluis8-tech <questluis8@gmail.com>
2025-08-14 22:53:43 +0100

    Create Minimal Test Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,9 @@
 import React from 'react';
-import { useGame } from './hooks/useGame';
-import { GameTable } from './components/GameTable';
-import { GameControls } from './components/GameControls';
-import { GameOverModal } from './components/GameOverModal';
 
 function App() {
-  const {
-    gameState,
-    selectedCards,
-    dealCards,
-    confirmFaceUpCards,
-    startGame,
-    handleCardClick,
-    playCards,
-    canPlaySelected,
-    pickupCards,
-    canPlayAnyCard,
-    playFaceDownCard
-  } = useGame();
-
   return (
-    <div className="min-h-screen bg-gradient-to-br from-red-900 via-red-800 to-orange-900 relative overflow-hidden">
-      {/* Background texture */}
-      <div className="absolute inset-0 opacity-20">
-        <div className="absolute inset-0 bg-gradient-to-br from-yellow-400/10 via-transparent to-red-600/10" />
-        <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,_transparent_0%,_rgba(0,0,0,0.3)_100%)]" />
-      </div>
-
-      <GameTable
-        gameState={gameState}
-        selectedCards={selectedCards}
-        onCardClick={handleCardClick}
-        onPlayFaceDownCard={playFaceDownCard}
-        canPlayAnyCard={canPlayAnyCard}
-      />
-
-      <GameControls
-        gameState={gameState}
-        selectedCards={selectedCards}
-        onDealCards={dealCards}
-        onConfirmFaceUpCards={confirmFaceUpCards}
-        onStartGame={startGame}
-        onPlayCards={playCards}
-        onPickupCards={pickupCards}
-        canPlaySelected={canPlaySelected}
-        canPlayAnyCard={canPlayAnyCard}
-      />
-
-      {gameState.gamePhase === 'finished' && (
-        <GameOverModal
-          winner={gameState.winner}
-          loser={gameState.loser}
-          players={gameState.players}
-          onNewGame={dealCards}
-        />
-      )}
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-white text-4xl">Test - Can you see this?</h1>
     </div>
   );
 }
