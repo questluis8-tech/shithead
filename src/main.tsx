cd27e429c4fcc5434d6da78dc749c2badc6602f4
questluis8-tech <questluis8@gmail.com>
2025-08-14 22:41:35 +0100

    Fix main.tsx to use App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Test } from './Test.tsx';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Test />
+    <App />
   </StrictMode>
 );
